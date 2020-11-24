b168b0b5e3d01be71fc03df94ac60ed496e745d0
hardbaited <50992941+hardbaited@users.noreply.github.com>
2020-11-24 19:59:29 +0200

    Update main.lua

--- esx_joblisting/server/main.lua
+++ esx_joblisting/server/main.lua
@@ -1,41 +1,5 @@
 ESX = nil
-TriggerEvent('ESX:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterServerCallback('ESX_joblisting:getJobsList', function(source, cb)
-	MySQL.Async.fetchAll('SELECT * FROM jobs WHERE whitelisted = @whitelisted', {
-		['@whitelisted'] = false
-	}, function(result)
-		local data = {}
-
-		for i=1, #result, 1 do
-			table.insert(data, {
-				job   = result[i].name,
-				label = result[i].label
-			})
-		end
-
-		cb(data)
-	end)
-end)
-
-
-RegisterServerEvent('ESX_joblisting:setJob')
-AddEventHandler('ESX_joblisting:setJob', function(job)
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-
-	MySQL.Async.fetchAll('SELECT whitelisted FROM jobs WHERE name = @name', {
-		['@name'] = job,
-	}, function(result)
-		if not result[1].whitelisted then
-			xPlayer.setJob(job, 0)
-		else
-			print(('ESX_joblisting: %s attempted to set a whitelisted job! (lua injector)'):format(xPlayer.identifier))
-		end
-		
-	end)
-
-end)
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 
 RegisterServerEvent('ESX_joblisting:Miner')
